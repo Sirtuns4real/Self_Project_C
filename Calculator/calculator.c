ab25972c0ad58bf1dcdae40e1c4b4d95e81a270e
Olatunde Oloyede <olatundeoloyede41@yahoo.com>
2022-12-20 21:06:16 +0100

    Create calculator.c

---
+++ Calculator/calculator.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "header.h"
+
+int main() 
+{
+    int num1, num2;
+    char name[50];
+    int selection;
+    
+    printf("This is my first calculator program\nPlease enter your name: ");
+    scanf("%[^\n]%*c", &name);
+    printf("\nWelcome to the calculator %s\nWhat operation would you like to perform: ", name);
+    operations();
+    printf("\nPlease Enter your Selection: ");
+    scanf("%i", &selection);
+    choice(selection);
+   
+    return (0);
+}
