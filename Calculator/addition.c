9415810c0207a8cfd1da573eab94b5c00607325e
Olatunde Oloyede <olatundeoloyede41@yahoo.com>
2022-12-20 20:56:20 +0100

    Create addition.c

---
+++ Calculator/addition.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int addition(void)
+{
+    int sum, num1, num2;
+    
+    printf("\nEnter the first number: "), scanf("%i", &num1);
+    printf("\nEnter the second number: "), scanf(" %i", &num2);
+    sum = num1 + num2;
+    printf("The sum of %i and %i is: %i", num1, num2, sum);
+    return (0);
+}
