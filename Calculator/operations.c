71a223cf0b26f86639882d0ac90af5419f65a547
Olatunde Oloyede <olatundeoloyede41@yahoo.com>
2022-12-20 21:04:18 +0100

    Create operations.c

---
+++ Calculator/operations.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib>
+#include "header.h"
+
+void operations(void)
+{
+    printf("\n1. Addition\n2. Subtraction\n3. Division\n4. Multiplication\n5. Modulo\n6. Power");
+}
