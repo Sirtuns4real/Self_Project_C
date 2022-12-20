40a185e2b08ce92a2eaa5218da094f4741387cc8
Olatunde Oloyede <olatundeoloyede41@yahoo.com>
2022-12-20 20:57:06 +0100

    Create subtraction.c

---
+++ Calculator/subtraction.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int subtraction(void)
+{
+    int sub, num1, num2;
+    
+    printf("\nEnter the first number: "), scanf("%i", &num1);
+    printf("\nEnter the second number: "), scanf(" %i", &num2);
+    sub = num1 - num2;
+    printf("The difference of %i and %i is: %i", num1, num2, sub);
+    return (0);
+}
