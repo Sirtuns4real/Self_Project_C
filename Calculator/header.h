e03cfb59b5983587a93b942a6fdc226e63cd34e9
Olatunde Oloyede <olatundeoloyede41@yahoo.com>
2022-12-20 20:53:14 +0100

    header

---
+++ Calculator/header.h
@@ -0,0 +1,13 @@
+ifndef main.h
+
+int addition(void);
+int subtraction(void);
+int division(void);
+int multiplication(void);
+int modulo(void);
+int power(void);
+int choice(int selection);
+void operations(void);
+void again(void);
+
+endif
