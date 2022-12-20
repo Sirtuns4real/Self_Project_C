6cc48c9084865713707217e80ff27ec315355ad2
Olatunde Oloyede <olatundeoloyede41@yahoo.com>
2022-12-20 21:00:11 +0100

    Create multiplication.c

---
+++ Calculator/multiplication.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "header.h"
+
+int multiplication(void)
+{
+    float multiply, num1, num2;
+    
+    printf("\nEnter the first number: "), scanf("%f", &num1);
+    printf("\nEnter the second number: "), scanf(" %f", &num2);
+    multiply = num1 * num2;
+    printf("The multiplication of %f by %f is: %f", num1, num2, multiply);
+    return (0);
+}
