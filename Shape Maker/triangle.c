8eb15e0cdeda869a4eb256b948ce1d6a93402fd6
Olatunde Oloyede <olatundeoloyede41@yahoo.com>
2022-12-25 23:37:57 +0100

    Create triangle.c

---
+++ Shape Maker/triangle.c
@@ -0,0 +1,20 @@
+int triangle(void)
+{
+   int i, space, rows, k = 0;
+   printf("\n Please Enter the number of rows: ");
+   scanf("%d", &rows);
+   for (i = 1; i <= rows; ++i) 
+   {
+      for (space = 1; space <= rows - i; ++space) 
+      {
+         printf("  ");
+      }
+      while (k != 2 * i - 1) 
+      {
+         printf("* ");
+         ++k;
+      }
+      printf("\n");
+   }
+   return (0);
+}
